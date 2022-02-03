4ef1444b5774f96f5a82373dc270f52f22dc75b8
JujuDeFruit <julien.raynal@yahoo.fr>
2022-02-02 22:00:21 -0500

    commit qui sert a rien

--- src/main/java/ca/uqac/API/controller/Connexion.java
+++ src/main/java/ca/uqac/API/controller/Connexion.java
@@ -3,6 +3,8 @@ package ca.uqac.API.controller;
 import ca.uqac.API.entity.Comptes;
 import ca.uqac.API.service.connexionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
