5f4ee7ea7896f8a4b547a54951d0edaff6cc830e
JujuDeFruit <julien.raynal@yahoo.fr>
2022-02-07 10:48:39 -0500

    Naming convention

--- src/main/java/ca/uqac/API/controller/Connexion.java
+++ src/main/java/ca/uqac/API/controller/Connexion.java
@@ -3,36 +3,40 @@ package ca.uqac.API.controller;
 import ca.uqac.API.entity.Comptes;
 import ca.uqac.API.service.connexionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8080/api")
-@RequestMapping("/connexion")
+@CrossOrigin(origins = "http://localhost:3000/")
+@RequestMapping("/api/connexion")
 public class Connexion {
     @Autowired
     connexionService connexionService;
 
-    @PostMapping("/new_account")
-    public @ResponseBody String add(@RequestBody Comptes comptes) {
-        connexionService.saveCompte(comptes);
-        return "Saved";
+    @PostMapping("/register")
+    public ResponseEntity add(@RequestBody Comptes comptes) {
+        try {
+            connexionService.saveCompte(comptes);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (DataIntegrityViolationException e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping("/login")
     public ResponseEntity<Comptes> ValidateConnection(@RequestParam("identifiant") String identifiant, @RequestParam("password") String password) {
         try {
             Comptes comptes = connexionService.getCompte(identifiant, password);
+            comptes.setPassword("");
             return new ResponseEntity<>(comptes, HttpStatus.OK);
         } catch (NoSuchElementException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
     }
 
---
+++ src/main/java/ca/uqac/API/controller/Google.java
@@ -0,0 +1,60 @@
+package ca.uqac.API.controller;
+
+import ca.uqac.API.entity.Comptes;
+import ca.uqac.API.utils.ParameterStringBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000/")
+@RequestMapping("/api/google")
+public class Google {
+    @GetMapping("/autocomplete")
+    public ResponseEntity<Comptes> AutoComplete(@RequestParam("input") String input, @RequestParam("key") String apiKey, @RequestParam("language") String lang) {
+        try {
+            Map<String, String> parameters = new HashMap<>();
+            parameters.put("input", input);
+            parameters.put("key", apiKey);
+            parameters.put("language", lang);
+
+            URL url = new URL("https://maps.googleapis.com/maps/api/place/autocomplete/json?".concat(ParameterStringBuilder.getParamsString(parameters)));
+
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setDoOutput(true);
+
+
+            int status = con.getResponseCode();
+            System.out.println(status);
+
+            BufferedReader in = null;
+
+            if (status > 299) {
+                in = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            } else {
+                in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            }
+
+
+            String inputLine;
+            StringBuilder content = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+            System.out.println(content);
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
